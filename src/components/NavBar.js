7b4d2b566f1e2aed24fb7cc7df47dcc8206aa1e4
maccabello <maca.cabello@hotmail.com>
2021-10-19 16:17:33 -0300

    Entrega 2

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+const Header = () => {
+    return(
+        <>
+        <nav id="navbar">
+            <h1>FN</h1>
+            <a href="index.html">Home</a>
+            <a href="#!">Sobre nosotrxs</a>
+        </nav>
+        <header id="main-header">
+                <h1>Futura Nostalgia</h1>
+                    <p>Celebramos lo viejo y lo nuevo de la música</p>
+                    <button>Remeras</button>
+                    <button>Buzos</button>
+                    <button>Camperas</button>
+                <button>Borrar filtros</button>
+        </header>
+        </>
+    )
+}
+export default Header 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-const Elemento = () => <p>Hola Mundo</p>
+import App from "./App";
+import "./estilos.css"
 
-ReactDOM.render( <Elemento/> , document.getElementById("root") )
+ReactDOM.render( <App/> , document.getElementById("root") )
