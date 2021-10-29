738b6ce75eee17cd1b0a3d3622d71d99729ee8e9
maccabello <maca.cabello@hotmail.com>
2021-10-28 22:36:22 -0300

    28/10

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,8 +6,8 @@ const ItemList = () => {
     useEffect(() => {
     setTimeout(() => {
     setProductos([
-        { id: 1, title: "Harry Styles", description: "buzo", precio: 4000, img: "http://placehold.it/100x100", stock: 18},
-        { id: 2, title: "David Bowie", description: "remera", precio: 2500, img: "http://placehold.it/100x100", stock: 13}
+        { id: 1, title: "Harry Styles", description: "buzo", precio: "$4000", img: "http://placehold.it/100x100", stock: 18},
+        { id: 2, title: "David Bowie", description: "remera", precio: "$2500", img: "http://placehold.it/100x100", stock: 13}
     ]);
     }, 2000);
     }, []);
@@ -21,7 +21,7 @@ const ItemList = () => {
         {Productos.map((e,i)=>{
             return (
             <>
-                <p key={e.id}>{e.title}</p>
+                <p key={e.id}>{e.title}  {e.description} {e.precio} {e.stock}</p>
                 <img key={i.id} alt='img' src={e.img}/>
                 <Item/>
             </>
